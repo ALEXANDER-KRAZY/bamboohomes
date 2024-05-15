e11d0a1a60d1af0f0bc160c2d2d039ac946dc424
ALEXANDER-KRAZY <alexandermutuku855@gmail.com>
2024-05-15 21:45:36 +0300

    Add grid for all screen sizes and gap of 8

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,14 @@ export default function Home() {
       <Container>
         <div className="
         pt-24
-        
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
         ">
           <div>
             my future listings
